a4242b47e0e8f63135ba0b253a2b1779fe0248b8
aco <petras.aleksandar49@gmail.com>
2024-08-05 09:29:06 +0200

    container3d margin on body click fix

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function applyMarginBasedOnChangeMobile() {
 
   if (isMobile) {
     if (isCheckedRadio) {
-        $("#container3d").css("margin-top", "-74px");
+        $("#container3d").css("margin-top", "-94px");
         $(".lower-btns").css({ "display": "none" });
 
     } else {
